e145b79c6c2412ca7287533cf26aa9ac81a8eef2
ujjawal jadhav <ujjawaljadhav03082002@gmail.com>
2026-08-16 15:58:08 +0530

    Global DI

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:getx_products/app/bindings/InitialBinding.dart';
-import 'package:getx_products/app/modules/product/controllers/cart_controller.dart';
 import 'package:getx_products/routes/app_pages.dart';
-import 'package:getx_products/services/cart_service.dart';
-import 'package:getx_products/services/product_service.dart';
 
 void main() async {
-  await GetStorage.init();  
-  Get.put(CartService(), permanent : true);
-  Get.put(ProductService());
-  Get.put(CartController());
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -21,7 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      
       initialBinding: InitialBinding(),
       debugShowCheckedModeBanner: false,
       title: 'Product App',
